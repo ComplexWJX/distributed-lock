4fea8c99355471ba2f9be60652180e845a3c4569
wengjx <wengjx@asiainfo.com>
2020-06-18 00:21:55 +0800

    1.死锁问题

--- src/main/java/com/asiainfo/distributed/sync/RedisLock.java
+++ src/main/java/com/asiainfo/distributed/sync/RedisLock.java
@@ -1,7 +1,10 @@
 package com.asiainfo.distributed.sync;
 
+import io.lettuce.core.RedisFuture;
+import io.lettuce.core.SetArgs;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.api.sync.RedisCommands;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -15,43 +18,73 @@ import lombok.extern.slf4j.Slf4j;
 public class RedisLock {
 
     private final StatefulRedisConnection connection = LettuceRedisPool.getInstance().getConnection();
-    RedisAsyncCommands<String,String> asyncCommands;
+    private RedisAsyncCommands<String,String> asyncCommands;
+    private RedisCommands<String,String> syncCommands;
     RedisLock(){
         asyncCommands = connection.async();
+        syncCommands = connection.sync();
     }
 
-    public void lock(){
-        asyncCommands.setex("lock",3000,"abc");
-        log.info("set key {}","lock success");
+    /***
+     * TODO 1.获取锁的注意点：客户端解锁要解自己加的锁，唯一标识？
+     *      2.设置key失效时间时down机，设置失败，key永不过期，解决方法：
+     *        （1）使用原子操作 set(k,v,nx|xx,ex|px) nxxx NX|XX, NX -- Only set the key if it does not already exist. XX -- Only set the key
+     *           if it already exist. expx EX|PX, expire time units: EX = seconds; PX = milliseconds
+     *         （2）Lua脚本实现 redis对lua脚本执行具有原子性
+     *
+     * @throws Exception
+     */
+    public void lock() throws Exception{
+        RedisFuture<Boolean> booleanRedisFuturefuture;
+        RedisFuture<String> stringRedisFuture;
+        Boolean isLock = false;
+        /*do{
+            // 异步操作setnx命令实现锁，通过阻塞获取future结果
+            booleanRedisFuturefuture = asyncCommands.setnx("lock","lock");
+            log.info("lockValue:{}",isLock);
+            Thread.sleep(1000);
+        }while (!(isLock =booleanRedisFuturefuture.get()));*/
+
+        do{
+            log.info("lockValue:{}",isLock);
+            SetArgs setArgs = new SetArgs().nx().ex(3);
+            stringRedisFuture = asyncCommands.set("lock", "lock", setArgs);
+            Thread.sleep(1000);
+        }while (!(isLock = ("OK".equals(stringRedisFuture.get()))));
+
+
+        log.info("========================"+Thread.currentThread().getName()+ "  get lock=====================");
+        //            asyncCommands.setex("lock",3000,"lock");
+        asyncCommands.expire("lock",3000); //TODO 如果执行到此处，服务器宕机，设置key失效时间失败，key永不过期，会造成死锁
     }
 
     public void unlock(){
         asyncCommands.del("lock");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         RedisLock redisLock = new RedisLock();
-        redisLock.unlock();
-//        new Thread(()->{
-//            try {
-//                System.out.println("t1 get lock");
-//                redisLock.lock();
-//                Thread.sleep(3000);
-//                redisLock.unlock();
-//                log.info("release lock {}","lock");
-//            }catch (InterruptedException ex){
-//                log.error(ex.getMessage());
-//            }
-//        }).start();
+        new Thread(()->{
+            try {
+                redisLock.lock();
+                Thread.sleep(3000);
+                redisLock.unlock();
+                log.info("t1 release lock {}","lock");
+            }catch (Exception ex){
+                log.error(ex.getMessage());
+            }
+        },"t1").start();
 
-//        new Thread(()->{
-//            try {
-//                System.out.println("t2 get lock");
-//                redisLock.lock();
-//            }finally {
-//                redisLock.unlock();
-//            }
-//        }).start();
+        new Thread(()->{
+            try {
+                redisLock.lock();
+                Thread.sleep(3000);
+                redisLock.unlock();
+                log.info("t2 release lock {}","lock");
+            }catch (Exception ex){
+                log.error(ex.getMessage());
+            }
+        },"t2").start();
 
     }
 }
--- src/test/java/com/asiainfo/distributed/DistributedApplicationTests.java
+++ src/test/java/com/asiainfo/distributed/DistributedApplicationTests.java
@@ -1,13 +1,25 @@
 package com.asiainfo.distributed;
 
+import com.asiainfo.distributed.config.RedisConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.security.RunAs;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DistributedApplicationTests {
 
+    @Autowired
+    private RedisConfig redisConfig;
+
     @Test
     void contextLoads() {
+        System.out.println(redisConfig.getHost());
+        System.out.println(redisConfig.getPassword());
     }
 
 }
